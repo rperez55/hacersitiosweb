ac2f3aaadfb01be7e3c0283bd5e16c5834bb67c9
Rodolfo Pérez <90464643+rperez55@users.noreply.github.com>
2026-03-18 16:59:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const modal = document.querySelector("#miPopup");
-const cerrar = document.querySelector("#cerrar");
-// Abrir automáticamente tras 3 segundos (3000ms)
-setTimeout(() => { modal.showModal(); }, 3000);
-cerrar.addEventListener("click", () => { modal.close(); });
